ab276cd77a84ba13ff3d6ff747ef8de1f611bcbc
Michael Bestas <mikeioannina@gmail.com>
2024-06-14 00:26:49 +0300

    sm6150-common: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

--- sm6150.mk
+++ sm6150.mk
@@ -273,7 +273,8 @@ PRODUCT_COPY_FILES += \
 
 # Network
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.1.vendor
+    android.system.net.netd@1.1.vendor \
+    libnetutils.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
@@ -316,7 +317,8 @@ PRODUCT_PACKAGES += \
     libjson \
     libril \
     librilutils \
-    librmnetctl
+    librmnetctl \
+    libsqlite.vendor
 
 # Recovery
 PRODUCT_PACKAGES += \
